955236a2695716e0700c16ff363843f5bb60192a
spockwang <spockwang@tencent.com>
2024-08-06 21:01:32 +0800

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import State (runTwoRandoms)
+import Regex (compile, match)
 
 main :: IO ()
 main = do
-  result <- runTwoRandoms
-  putStrLn (show result)
+  regex <- compile (pack "quick brown") []
+  matches <- match regex (pack "quick brown box") []
+  putStrLn (show matches)
