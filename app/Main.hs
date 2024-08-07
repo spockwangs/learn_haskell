e3d40c236b48b78f91f42ba41ff94d83aadd1f8a
spockwang <spockwang@tencent.com>
2024-08-07 12:42:20 +0800

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,23 @@ module Main where
 
 import State (runTwoRandoms)
 import Regex (compile, match)
+import Data.ByteString.Char8 (pack)
+import Data.ByteString (ByteString)
 
 main :: IO ()
 main = do
-  regex <- compile (pack "quick brown") []
-  matches <- match regex (pack "quick brown box") []
-  putStrLn (show matches)
+  let regexPattern = (pack "quick brown")
+      str = (pack "quick brown box")
+      matches = matchStr regexPattern str
+  case matches of
+    Left error -> putStrLn (show error)
+    Right result -> putStrLn (show result)
+  where
+    matchStr :: ByteString -> ByteString -> Either String [ByteString]
+    matchStr pat s = do
+      regex <- compile pat []
+      morphEither $ match regex s []
+        where
+          morphEither :: Maybe a -> Either String a
+          morphEither Nothing = Left "Nothing"
+          morphEither (Just x) = Right x
