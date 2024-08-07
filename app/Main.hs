9273d916360cdda345da4ba4211e8511b325370c
spockwang <spockwang@tencent.com>
2024-08-07 12:54:18 +0800

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -8,17 +8,17 @@ import Data.ByteString (ByteString)
 main :: IO ()
 main = do
   let regexPattern = (pack "quick brown")
-      str = (pack "quick brown box")
+      str = (pack "quick brow box")
       matches = matchStr regexPattern str
   case matches of
-    Left error -> putStrLn (show error)
+    Left err -> putStrLn (show err)
     Right result -> putStrLn (show result)
   where
     matchStr :: ByteString -> ByteString -> Either String [ByteString]
     matchStr pat s = do
       regex <- compile pat []
-      morphEither $ match regex s []
+      match regex s [] `orDie` "not matched"
         where
-          morphEither :: Maybe a -> Either String a
-          morphEither Nothing = Left "Nothing"
-          morphEither (Just x) = Right x
+          orDie :: Maybe a -> String -> Either String a
+          Nothing `orDie` err = Left err
+          (Just x) `orDie` _ = Right x
